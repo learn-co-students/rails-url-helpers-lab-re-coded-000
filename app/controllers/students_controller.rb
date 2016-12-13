95d6fdb651be81401f293470ec2cd1ef9fff7768
ALI ALRAWI <ali.developer90@gmail.com>
2016-12-13 06:52:05 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,7 @@ class StudentsController < ApplicationController
   end
   def activate
     @student = Student.find(params[:id])
-    @student.active = !@student.active
+    @student.active ? @student.active=false : @student.active=true
     @student.save
     redirect_to student_path(@student)
   end
